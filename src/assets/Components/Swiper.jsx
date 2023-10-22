a727dd7d3b622bac0e368d371b2d27053842282e
andresRivera123 <carlosandresriveravalle@gmail.com>
2023-10-23 00:59:00 +0200

    Knowledge: Carrusel funcional

---
+++ src/assets/Components/Swiper.jsx
@@ -0,0 +1,181 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "../Styles/Components/swiper.css";
+import serviceBasic from "../Images/ServiceImage1.jpg";
+import nextIcon from "../Images/Icons/NextIcon.svg";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
+export default function SwiperExample() {
+  return (
+    <>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        breakpoints={{
+          180: {
+            slidesPerView: 1,
+            spaceBetween: 15,
+          },
+          550: {
+            slidesPerView: 1,
+            spaceBetween: 15,
+          },
+          750: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+          },
+          1050: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+          },
+          1500: {
+            slidesPerView: 4,
+            spaceBetween: 15,
+          },
+        }}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="swiper__container">
+            <div className="swiper__first">
+              <img src={serviceBasic} alt="Image" className="swiper__image" />
+              <h2 className="swiper__paragraph basictitle">
+                Basic General Services
+              </h2>
+            </div>
+            <p className="paragraph">
+              El servicio lorem servicio lorem servicio lorem servicio lorem
+              servicio lorem servicio lorem servicio lorem
+            </p>
+            <img src={nextIcon} alt="Next Icon" className="swiper__next" />
+
+
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="swiper__container">
+            <div className="swiper__first">
+              <img src={serviceBasic} alt="Image" className="swiper__image" />
+              <h2 className="swiper__paragraph basictitle">
+                Basic General Services
+              </h2>
+            </div>
+            <p className="paragraph">
+              El servicio lorem servicio lorem servicio lorem servicio lorem
+              servicio lorem servicio lorem servicio lorem
+            </p>
+            <img src={nextIcon} alt="Next Icon" className="swiper__next" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="swiper__container">
+            <div className="swiper__first">
+              <img src={serviceBasic} alt="Image" className="swiper__image" />
+              <h2 className="swiper__paragraph basictitle">
+                Basic General Services
+              </h2>
+            </div>
+            <p className="paragraph">
+              El servicio lorem servicio lorem servicio lorem servicio lorem
+              servicio lorem servicio lorem servicio lorem
+            </p>
+            <img src={nextIcon} alt="Next Icon" className="swiper__next" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="swiper__container">
+            <div className="swiper__first">
+              <img src={serviceBasic} alt="Image" className="swiper__image" />
+              <h2 className="swiper__paragraph basictitle">
+                Basic General Services
+              </h2>
+            </div>
+            <p className="paragraph">
+              El servicio lorem servicio lorem servicio lorem servicio lorem
+              servicio lorem servicio lorem servicio lorem
+            </p>
+            <img src={nextIcon} alt="Next Icon" className="swiper__next" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="swiper__container">
+            <div className="swiper__first">
+              <img src={serviceBasic} alt="Image" className="swiper__image" />
+              <h2 className="swiper__paragraph basictitle">
+                Basic General Services
+              </h2>
+            </div>
+            <p className="paragraph">
+              El servicio lorem servicio lorem servicio lorem servicio lorem
+              servicio lorem servicio lorem servicio lorem
+            </p>
+            <img src={nextIcon} alt="Next Icon" className="swiper__next" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="swiper__container">
+            <div className="swiper__first">
+              <img src={serviceBasic} alt="Image" className="swiper__image" />
+              <h2 className="swiper__paragraph basictitle">
+                Basic General Services
+              </h2>
+            </div>
+            <p className="paragraph">
+              El servicio lorem servicio lorem servicio lorem servicio lorem
+              servicio lorem servicio lorem servicio lorem
+            </p>
+            <img src={nextIcon} alt="Next Icon" className="swiper__next" />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
+
+/* breakpoints={{
+          180: {
+            slidesPerView: 1,
+            spaceBetween: 15,
+          },
+          550: {
+            slidesPerView: 1,
+            spaceBetween: 15,
+          },
+          750: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+          },
+          1050: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+          },
+          1500: {
+            slidesPerView: 4,
+            spaceBetween: 15,
+          },
+        }} */
+
+/*  <div className="swiper__container">
+            <div className="swiper__first">
+              <img src={serviceBasic} alt="Image" className="swiper__image" />
+              <h2 className="swiper__paragraph basictitle">Basic General Services</h2>
+            </div>
+            <p className="paragraph">
+              El servicio lorem servicio lorem servicio lorem servicio lorem
+              servicio lorem servicio lorem servicio lorem
+            </p>
+            <img src={nextIcon} alt="Next Icon" className="swiper__next" />
+          </div> */
--- src/assets/Pages/Home.jsx
+++ src/assets/Pages/Home.jsx
@@ -1,10 +1,10 @@
 import Navbar from "../Components/Navbar";
+import SwiperExample from "../Components/Swiper";
 import logo from "../Images/Giss-Logo.png";
-import serviceIcon1 from "../Images/Icons/ServiceIcon1.png"
-import serviceIcon2 from "../Images/Icons/ServiceIcon2.png"
-import serviceIcon3 from "../Images/Icons/ServiceIcon3.png"
-import flag from "../Images/Icons/flagIcon.png"
-
+import serviceIcon1 from "../Images/Icons/ServiceIcon1.png";
+import serviceIcon2 from "../Images/Icons/ServiceIcon2.png";
+import serviceIcon3 from "../Images/Icons/ServiceIcon3.png";
+import flag from "../Images/Icons/flagIcon.png";
 
 function HomePage() {
   return (
@@ -15,60 +15,56 @@ function HomePage() {
         <section className="hero ">
           {/*  <img src={logo} alt="Background hero Giss" /> */}
           <h1 className="hero__title">Dont’t clean just call us</h1>
-          <p className="hero__paragraph">Limpieza de hogares y oficinas a domicilio en New Jersey</p>
+          <p className="hero__paragraph">
+            Limpieza de hogares y oficinas a domicilio en New Jersey
+          </p>
           <button className="quote__button">Get a free quote</button>
         </section>
         {/* -- INFORMATION -- */}
-        <section className="information">
+        <section className="  ">
           <article className="service__icons container">
             <div className="service__icon">
-              <img
-                src={serviceIcon1}
-                alt="Service icon 1"
-                className="icon"
-              />
+              <img src={serviceIcon1} alt="Service icon 1" className="icon" />
               <h2 className="basictitle">Limpieza personalizada</h2>
-              <p className="paragraph  aligncenter">Obtén una limpieza personalizada para tu hogar u oficina.</p>
+              <p className="paragraph  aligncenter">
+                Obtén una limpieza personalizada para tu hogar u oficina.
+              </p>
             </div>
             <div className="service__icon">
-              <img
-                src={serviceIcon2}
-                alt="Service icon 2"
-                className="icon"
-              />
+              <img src={serviceIcon2} alt="Service icon 2" className="icon" />
               <h2 className="basictitle">Servicios Profesionales</h2>
-              <p className="paragraph  aligncenter">Nuestros servicios profesionales garantizan una limpieza minuciosa y de alta calidad.</p>
+              <p className="paragraph  aligncenter">
+                Nuestros servicios profesionales garantizan una limpieza
+                minuciosa y de alta calidad.
+              </p>
             </div>
             <div className="service__icon">
-              <img
-                src={serviceIcon3}
-                alt="Service icon 3"
-                className="icon"
-              />
+              <img src={serviceIcon3} alt="Service icon 3" className="icon" />
               <h2 className="basictitle">Comunicación facil</h2>
-              <p className="paragraph  aligncenter">Mantente en contacto con nuestro equipo y recibe información de los servicios y cotización.</p>
+              <p className="paragraph  aligncenter">
+                Mantente en contacto con nuestro equipo y recibe información de
+                los servicios y cotización.
+              </p>
             </div>
           </article>
           {/* KNOWLEDGE */}
           <section className="knowledge">
             <div className="knowledge__container container">
-              <h2 className="subtitle">Nuestros servicios de limpieza de hogar</h2>
-              <div className="knowledge__services">
-
-
-              </div>
-
+              <h2 className="subtitle">
+                Nuestros servicios de limpieza
+              </h2>
+              <SwiperExample/>
+              <div className="knowledge__services"></div>
             </div>
-
           </section>
           <div className="service__container container">
             <h2 className="subtitle">What is GISS CLEANING SERVICES?</h2>
             <p className="paragraph">
-              Somos una compañía que ofrece servicios profesionales de
-              limpieza y mantenimiento para hogares, oficinas, estudios con el
-              objetivo de entregar una confianza hacia el cliente y satisfacer
-              las necesidades de cada uno, ofreciendo seguridad y confianza
-              para que su sitio quede impecablemente limpio.
+              Somos una compañía que ofrece servicios profesionales de limpieza
+              y mantenimiento para hogares, oficinas, estudios con el objetivo
+              de entregar una confianza hacia el cliente y satisfacer las
+              necesidades de cada uno, ofreciendo seguridad y confianza para que
+              su sitio quede impecablemente limpio.
             </p>
 
             <a href="#" className="information__readmore transition1">
@@ -89,7 +85,6 @@ function HomePage() {
             ></iframe>
           </div>
         </section>
-
       </main>
     </>
   );
